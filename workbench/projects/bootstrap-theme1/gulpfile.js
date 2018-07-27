c94c6c395d0e3e3ac89212e050c33321ab0645b0
Zed-M <fsociety.2017@protonmail.com>
2018-07-27 12:06:49 +0100

    new update

--- workbench/projects/bootstrap-theme1/gulpfile.js
+++ workbench/projects/bootstrap-theme1/gulpfile.js
@@ -1,43 +1,59 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
 
-// Compile Sass & Inject Into Browser
-gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+gulp.task('compile-sass', function () {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css/'))
+    .pipe(browserSync.stream())
+})
 
-// Move JS Files to src/js
-gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/js"))
-        .pipe(browserSync.stream());
-});
+gulp.task('minify-css', function () {
+  gulp.src('./src/css/*.css')
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./src/minify/'))
+})
 
-// Watch Sass & Serve
-gulp.task('serve', ['sass'], function() {
+gulp.task('move-js', function () {
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream())
+})
 
-    browserSync.init({
-        server: "./src"  
-    });
-
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
-
-// Move Fonts to src/fonts
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
+gulp.task('move-fonts', function () {
+  return gulp.src(['node_modules/font-awesome/fonts/*'])
     .pipe(gulp.dest('src/fonts'))
+    .pipe(browserSync.stream())
 })
 
-// Move Font Awesome CSS to src/css
-gulp.task('fa', function() {
-  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+gulp.task('move-css-fonts', function () {
+  return gulp.src(['node_modules/font-awesome/css/font-awesome.min.css'])
     .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
 })
 
-gulp.task('default', ['js','serve', 'fa', 'fonts']);
+// run sass when serve runs
+// run server
+// watch for any changes in src/scss folder and reload the browser
+// also watch for sass changes
+// watch for html changes
+gulp.task('launch-server', ['compile-sass'], function () {
+  browserSync.init({
+    server: './src'
+    // proxy: 'http://localhost/project/code%20design/dzcode.design/bootstrap%20workspace/1st%20udemy/src'
+  })
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss',
+    'node_modules/font-awesome/scss/font-awesome.scss',
+    'src/scss/*.scss'
+  ], ['compile-sass'])
+  gulp.watch('src/*.html').on('change', browserSync.reload)
+})
+
+// run gulp
+// launch server and browser
+// execute js task
+gulp.task('default', ['minify-css', 'move-js', 'move-fonts', 'move-css-fonts', 'launch-server'])
