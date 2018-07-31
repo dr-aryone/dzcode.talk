466000ad148b80b0e93d04d0dd7599da39e6e0f4
Zed-M <fsociety.2017@protonmail.com>
2018-07-31 17:25:12 +0100

    final bookmark.js and new convert file

--- workbench/javascript-traversey/bookmark.js
+++ workbench/javascript-traversey/bookmark.js
@@ -7,6 +7,10 @@ function saveBookmark(e) {
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
+  if (!validateForm(siteName, siteUrl)) {
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl,
@@ -40,16 +44,89 @@ function saveBookmark(e) {
   // Clear form
   document.getElementById('myForm').reset();
 
+  // Re-fetch bookmarks
+  fetchBookmarks();
+
   // Prevent form from submitting
   e.preventDefault();
 }
-//fetch bookmarks
 
-function fetchBookmarks() {
+// Delete bookmark
+function deleteBookmark(url) {
+  // Get bookmarks from localStorage
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-  // console.log(bookmarks);
-  //get output id
-  var bookmarksResults = document.getElementById('bookmarksResults');
-  bookmarksResults.innerHTML += 'hello';
-  
+  // Loop through the bookmarks
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url == url) {
+      // Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
 }
+
+// Fetch bookmarks
+function fetchBookmarks() {
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
+
+  // Build output
+  bookmarksResults.innerHTML = '';
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML +=
+      '<div class="card jumbotron text-left">' +
+      '<h3>' +
+      name +
+      ' <a class="btn btn-light" target="_blank" href="' +
+      addhttp(url) +
+      '"> Visit </a> ' +
+      ' <a onclick="deleteBookmark(\'' +
+      url +
+      '\')" class="btn btn-danger" href="#"> Delete </a> ' +
+      '</h3>' +
+      '</div>';
+  }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
+}
+
+function addhttp(url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+    url = 'http://' + url;
+  }
+  return url;
+}
+
+//   bookmarksResults.innerHTML +=
+//     '<div class="card jumbotron text-left"><h3>' +
+//     name +
+//     '<a class="btn btn-light" target="_blank" href="' +
+//     url +
+//     '"> Visit </a> <a class="btn btn-danger" href="#" onclick="deleteBookmark(\'' +
+//     url +
+//     '\')"> Delete </a> </h3> </div>'; }
