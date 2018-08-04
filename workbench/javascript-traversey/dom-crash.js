504a40936f20dcbc03ea2f3d98c0664868c98d95
Zed-M <fsociety.2017@protonmail.com>
2018-08-04 13:07:02 +0100

    new update

--- workbench/javascript-traversey/dom-crash.js
+++ workbench/javascript-traversey/dom-crash.js
@@ -1,13 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-// var filter = document.getElementById('filter');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
-// // Delete event
-// itemList.addEventListener('click', removeItem);
-// // Filter event
-// filter.addEventListener('keyup', filterItems);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item
 function addItem(e) {
@@ -39,29 +39,29 @@ function addItem(e) {
   itemList.appendChild(li);
 }
 
-// // Remove item
-// function removeItem(e) {
-//   if (e.target.classList.contains('delete')) {
-//     if (confirm('Are You Sure?')) {
-//       var li = e.target.parentElement;
-//       itemList.removeChild(li);
-//     }
-//   }
-// }
+// Remove item
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
 
-// // Filter Items
-// function filterItems(e) {
-//   // convert text to lowercase
-//   var text = e.target.value.toLowerCase();
-//   // Get lis
-//   var items = itemList.getElementsByTagName('li');
-//   // Convert to an array
-//   Array.from(items).forEach(function(item) {
-//     var itemName = item.firstChild.textContent;
-//     if (itemName.toLowerCase().indexOf(text) != -1) {
-//       item.style.display = 'block';
-//     } else {
-//       item.style.display = 'none';
-//     }
-//   });
-// }
+// Filter Items
+function filterItems(e) {
+  //   convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  //   Get lis
+  var items = itemList.getElementsByTagName('li');
+  //   Convert to an array
+  Array.from(items).forEach(function(item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
