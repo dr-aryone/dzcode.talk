f72ae0169499fa645cfa35731f19b911dc835c61
Zed-M <fsociety.2017@protonmail.com>
2019-02-08 22:09:59 +0100

    cookies functionality in js

--- MyGoToFolder/derek.js
+++ MyGoToFolder/derek.js
@@ -26,7 +26,110 @@ for in & for of notew:
 *.to allow them. Separately, loops should be avoided in favor of array iterations.
 
 variables should always be declared in the top of the script
+
+cookies and validation are not to do with js cause it is
+easily intercepted with a proxy intercepter (hacking tool) so u should be aware of that.
 */
+
+/* arrays, numbers & strings functions */
+/*
+number : convert the element to a number (integer)
+parseInt : convert the element to an integer
+String : change the element to a string
+toFixed : shorten a float to a fixed integer
+typeof : returns the datatype of the element
+Boolean : false(0 or Nan : not a number) , true
+
+min,max,charAt,indexOf, Math.random().
+
+string.replace("Omar","amin");  // changing the given string in the 1st parm by the 2cd param
+string.trim(); // delete trim spaces from the start to the end
+string.split(separator, limit) : lastName.split("/",2) split the string to words with / separting
+them and only take the 2 first elements only
+string.repeat(num) : repeat the string
+string.substr(startindex,numberofcharacters) : returns part of a str
+string.subString(startindex,endindex) : returns part of a str
+
+array.push(x) : // add an element to the end
+array.pop() : // remove the last element and returns it
+array.shift() : // remove the first element of the array and return it
+array.unshift(x) : add element to the begininng of the array and retun its length
+array.reverse(); // Returns the array reversed
+array.sort(); // Returns the array sorted
+array.concat(arr2); // Join 2 arrays
+array.join(" "); // Converts array into a string and split the words with a space
+array.slice(0,2); // Returns part of an array same functionalty of substring
+
+array.splice(startIndex, howManyItemsUwantToDelete,replacableitem1,replacableitem2...) :
+can delete & insert items in an array
+
+array.splice(0,3); // remove items(splice) from the index 0 the 3 next elements
+
+array.splice(0,3,9,10,11); // this will replace the 3 elements from the start
+with the rest of the elements given after
+
+*/
+// events
+/*
+    ondblclick : ondblclick
+    onmouseover : mousehover
+    onmouseout : mouse leave
+    onresize : when u resize the browser
+    onload : when the page loads
+    onBlur : when u get out of the element
+    onFocus : when u r in the element (using it)
+    onChange : if you changed the default behavior of the element selected it will work
+    OnAbort - Called when a page load is interrupted
+    OnError - Called when an error occurs during page load
+    OnKeyDown - When key is pressed but not released
+    OnKeyUp - When key is released
+    OnMouseUp - When mouse button is released
+    OnReset - When reset button is clicked
+    OnSelect - When text is selected
+    OnSubmit - When submit button is clicked
+    OnUnload - When user leaves a page
+    */
+
+/* DOM :
+innerHTML its a complete tag "<p>ccc</p>"" while textContent its the object of the tag the corp "ccc" ,
+oncontextmenu when you enter into input
+
+lets say we have this <div> <p>x<f> <br> <i>y</i> </div>
+
+parentNode : the parent element here is the div if we dont have nothing then it is the body
+firstChild : p , lastChild : i, childNodes[0 - n] : p br (which means even the spaces are counted) i ,removeChild
+nextElementSibling,previousElementSibling
+insertBefore(beforethis, insertThis), insertAfter(afterthis, insertThis)
+nodeValue,
+createElement,createTextNode => appendChild : when we create an element to make him alive in the page we append
+
+alert(img[0].src);  : quick tip alert of the value of the source
+
+lien[0].setAttribute('href', 'https://www.facebook.com'); : modify a certain attribut
+lien[0].getAttribute('href'); : to get the value of a specific attribut
+
+e.preventDefault() :
+it tells the browser that if there is a default behavior for this event on this object, then skip that default behavior.
+
+So, for example, if you had a submit button that the default
+behavior was to submit a form and you had a click handler on that button that did a preventDefault(),
+then the browser would not submit the form when the button was clicked.
+ A classic use of this might be when the form doesn't validate so you show the user an error message
+and don't want the form to be submitted to the server.
+
+Or another example. If you set up a click handler for a link and you call e.preventDefault()
+in that click handler, then the browser will not process the click on the link and will not follow the href in the link.
+
+setTimeout  : setTimeout(function(){	d.style.backgroundColor="red"; },500);
+setInterval , ClearInterval  :   setInterval : x=setInterval(fonctionx,1000);  , clearInterval(x);
+
+*/
+/* cookies */
+// Cookies are used to store information on the clients hard drive
+// You can only access the information you stored on the clients machine
+// You can’t access other websites cookies
+// Cookies contain a name, value, expiration, directory that can access it and the domain that can access it
+
 const escapedChars = '" \' \\ \t \n';
 const lastName = 'hammou';
 const firstName = `Amine ${lastName}`;
@@ -38,10 +141,15 @@ const state = 'blida';
 // condition is x if true ? do this : else do this
 const testConditional = 4 < 10 ? '4 is less' : '4 is big';
 
+const regex = /[A-Za-z0-9.-_]+@[A-Za-z]+\.[A-Za-z]{2,3}/;
+// /regex/g that g means iterates all the regex founded
+const email = 'amine.kacem98@gmail.com';
+const divide = document.formx.num2;
+
 let keyClicked = 0;
 let mouseX = 0;
 let mouseY = 0;
-// we did a funcrion downthere that logs all the mouvement of the mouse and keys so we will change
+// we did a funcrion down there that logs all the mouvement of the mouse and keys so we will change
 // the default attitude of these special functions to reasign them those collected values
 document.onkeypress = keyPressed;
 document.onmousemove = mousePos;
@@ -71,40 +179,6 @@ while (num < 10) {
   continue;
   // continue get out of everything except the loop it will ignore what is down and get back from the while start
 }
-
-/* arrays, numbers & strings functions */
-/*
-number : convert the element to a number (integer)
-parseInt : convert the element to an integer
-String : change the element to a string
-toFixed : shorten a float to a fixed integer
-typeof : returns the datatype of the element
-Boolean : false(0 or Nan : not a number) , true
-
-min,max,charAt,indexOf, Math.random().
-
-string.replace("Omar","amin");  // changing the given string in the 1st parm by the 2cd param
-string.trim(); // delete trim spaces from the start to the end
-array.push("racha"); // insert this item at the end of the array
-
-string.split(separator, limit) : lastName.split("/",2) split the string to words with / separting
-them and only take the 2 first elements only
-
-array.reverse(); // Returns the array reversed
-array.sort(); // Returns the array sorted
-array.concat(arr2); // Join 2 arrays
-array.join(" "); // Converts array into a string and split the words with a space
-array.slice(0,2); // Returns part of an array same functionalty of substring
-
-array.splice(startIndex, howManyItemsUwantToDelete,replacableitem1,replacableitem2...) :
-can delete & insert items in an array
-
-array.splice(0,3); // remove items(splice) from the index 0 the 3 next elements
-
-array.splice(0,3,9,10,11); // this will replace the 3 elements from the start
-with the rest of the elements given after
-
-*/
 
 // it will sort the array from 1 2 3 4 5 6 and then it will log out the elements that were deleted
 console.log(`we deleted these ${arr.sort().splice(0, 3)} the new array is ${arr}`);
@@ -145,27 +219,6 @@ for (const i of vehicules) {
 for (const [key, value] of vehicules) {
   document.write(`<br> the key is ${key} & the value is ${value} <br> `);
 }
-
-// events
-/*
-    ondblclick : ondblclick
-    onmouseover : mousehover
-    onmouseout : mouse leave
-    onresize : when u resize the browser
-    onload : when the page loads
-    onBlur : when u get out of the element
-    onFocus : when u r in the element (using it)
-    onChange : if you changed the default behavior of the element selected it will work
-    OnAbort - Called when a page load is interrupted
-    OnError - Called when an error occurs during page load
-    OnKeyDown - When key is pressed but not released
-    OnKeyUp - When key is released
-    OnMouseUp - When mouse button is released
-    OnReset - When reset button is clicked
-    OnSelect - When text is selected
-    OnSubmit - When submit button is clicked
-    OnUnload - When user leaves a page
-    */
 
 /* mouse keyboard position and clicked value trick */
 
@@ -235,43 +288,10 @@ function changenode() {
 function changehtml() {
   document.querySelector('h3').innerHTML += ' and goodbye world';
 }
-/* DOM :
-innerHTML its a complete tag "<p>ccc</p>"" while textContent its the object of the tag the corp "ccc" ,
-oncontextmenu when you enter into input
 
-lets say we have this <div> <p>x<p> <br> <i>y</i> </div>
+// lets play a litle bit with node elements
 
-parentNode : the parent element here is the div if we dont have nothing then it is the body
-firstChild : p , lastChild : i, childNodes[0 - n] : p br (which means even the spaces are counted) i ,removeChild
-nextElementSibling,previousElementSibling
-insertBefore(beforethis, insertThis), insertAfter(afterthis, insertThis)
-nodeValue,
-createElement,createTextNode => appendChild : when we create an element to make him alive in the page we append
-
-alert(img[0].src);  : quick tip alert of the value of the source
-
-lien[0].setAttribute('href', 'https://www.facebook.com'); : modify a certain attribut
-lien[0].getAttribute('href'); : to get the value of a specific attribut
-
-
-e.preventDefault() :
-
-it tells the browser that if there is a default behavior for this event on this object, then skip that default behavior.
-
-So, for example, if you had a submit button that the default
-behavior was to submit a form and you had a click handler on that button that did a preventDefault(),
-then the browser would not submit the form when the button was clicked.
- A classic use of this might be when the form doesn't validate so you show the user an error message
-and don't want the form to be submitted to the server.
-
-Or another example. If you set up a click handler for a link and you call e.preventDefault()
-in that click handler, then the browser will not process the click on the link and will not follow the href in the link.
-
-setTimeout  : setTimeout(function(){	d.style.backgroundColor="red"; },500);
-setInterval , ClearInterval  :   setInterval : x=setInterval(fonctionx,1000);  , clearInterval(x);
-
-*/
-document.querySelector('div').addEventListener('click', nodeFunction());
+document.querySelector('div').addEventListener('click', nodeFunction);
 function nodeFunction() {
   const div = document.querySelector('div');
   const hr = document.createElement('hr');
@@ -286,6 +306,126 @@ function nodeFunction() {
   div.appendChild(text);
   div.appendChild(hr);
 }
+
+/* Regex its basicly like every other lang just the methods are different */
+
+document.write(`${email.match(regex)} <br>`); // it will give you the index of the match
+document.write(`${email.search(regex)} <br>`); // boolean search
+document.write(`${email.replace(regex, 'amine.kacem2015@gmail.io')} <br>`);
+document.write(`${regex.test(email)} <br>`); // test if the email is aquirent to the regex
+
+/* validation of an email through an input and a span to log */
+
+// this function basicly just check if there is anything on the span area then it empty it to log the text we want
+function emptySpan(spanId) {
+  if (spanId != null)
+    // while there is an element(child) delete it
+    while (spanId.firstChild) spanId.removeChild(spanId.firstChild);
+}
+// this is the main function that do all the work it checks the regex of the input (email)
+// & it logs out the error text into the span.
+function nodeTextLog(regEXP, input, spanId, text) {
+  // we test that the input value is a wrong email
+  if (!regEXP.test(input)) {
+    // if there is value in the span then we delete it
+    // then we log the error message
+    emptySpan(spanId);
+    spanId.appendChild(document.createTextNode(text));
+    return false;
+  }
+  // there is no error so we just
+  // delete the span without loging the error
+  emptySpan(spanId);
+  return true;
+}
+// then we simply call that function with our error msg we can also
+// use it with validating number or idk we just change the regex and the msg lol
+function validateEmail(input, spanId) {
+  return nodeTextLog(regex, input.value, spanId, 'your email is wrong');
+}
+
+/* error handling */
+
+divide.addEventListener('blur', dividingByZero);
+
+/* so this function basicly do the division and handles the error that comes from it
+and we gonna use the span to either launch an error message or just show the divison result */
+
+function dividingByZero() {
+  const spanID = document.getElementById('answer');
+  const num1 = document.formx.num1.value;
+  const num2 = document.formx.num2.value;
+  // we use try to do our code and catch to handle the error
+  try {
+    if (!num1 || !num2) throw new Error('please put values');
+    else if (num2 === 0) throw new Error('you cant devide on zero');
+    // isNan : boolean function that verifs if it is a number
+    else if (isNaN(num1) || isNaN(num2)) throw new Error('please enter a real number');
+    // like before we delete anything in the spanid
+    emptySpan(spanID);
+    // then if everything is correct we echo out the correct answer
+    spanID.appendChild(document.createTextNode(num1 / num2));
+  } catch (errorMsg) {
+    // like before we delete anything in the spanid just in case
+    emptySpan(spanID);
+    // then we just echo the error msg
+    spanID.appendChild(document.createTextNode(errorMsg));
+  }
+}
+// cookies
+
+// function getcookie :  to get cookies we need the dom document.cookie then we split them into an array to verify
+// if the input value is equals to a cookiename if so then we trim the white spaces from the cookie data
+// then grab the cookie of the input name.
+document.formx.getcookie.addEventListener('blur', () => {
+  // the cookie name we want to look for
+  const cookiename = document.formx.getcookie.value;
+  // all the cookies saved in the dom
+  // we split them to get a better look on a defined array of cookies
+  const allcookies = document.cookie.split(';');
+  let match = '';
+  let cookiedata = '';
+  for (let i = 0; i < allcookies.length; i++) {
+    // we verify if the cookie name does exists in allcookies
+    if (allcookies[i].indexOf(cookiename) !== -1) {
+      // if true then we remove the extra white spaces
+      match = allcookies[i].replace(' ', '');
+      // then get the cookiedate by taking a substring === cookiename from allcokies
+      // +1 is to avoid the =
+      cookiedata = match.substr(cookiename.length + 1);
+      alert(`${cookiename} ${cookiedata}`);
+    }
+  }
+});
+
+document.formx.namecookie.addEventListener('blur', createcookie);
+
+// remember a cookie contain : name ,value , expirationdate,
+// path(if it starts with a dot . means the subfolders/files are included to the cookie), domain
+// expl : cookiename=firstname;path=./var/lib;expires=12 JAN 2019;
+// so this function will get the cookievalue and create a date and assing it to document.cookie simple as that
+function createcookie(cookiename) {
+  const cookievalue = document.formx.namecookie.value;
+  cookiename = 'firstname';
+  // cookiename = document.formx.namecookie;
+  // we get the current date then we set the month value to 1 month ahead
+  // then convert it to a GMTstring (localtime writing cookie default GMT)
+  const expirationDate = new Date();
+  expirationDate.setMonth(expirationDate.getMonth() + 1);
+  document.cookie = `${cookiename}=${cookievalue};path=/;expires=${expirationDate};`;
+}
+
+// function delete cookie : this function locate the cookie with the name remve the value and expires the date
+document.formx.deletecookie.addEventListener('blur', () => {
+  const cookiename = document.formx.deletecookie.value;
+  // a trick to delete a cookie we call the create cookie function
+  // the first param is the new name to delete , the second we empty up the cookievalue ,
+  // the third we changeback the date to expire it
+  return createcookie(cookiename, '', -1);
+});
+
+console.log(document.cookie);
+document.write(`<h1> every cookie : ${document.cookie} </h1>`);
 
 /* OOP */
 
