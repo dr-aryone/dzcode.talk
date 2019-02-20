3325eca4f41bc76eec27a2ddc4614c880f9e110f
Zed-M <fsociety.2017@protonmail.com>
2019-02-20 13:42:33 +0100

    async await updated

--- test/motivation.js
+++ test/motivation.js
@@ -3,23 +3,53 @@
 
 const apiKey = "45f7a5210c9a4f2bcba7438ea1d65f3c959502f211011ab634276f7d034fe32d";
 const grid = document.getElementById("grid");
+const div = document.getElementById("unsplash");
 const imageContent = document.getElementById("image-content");
 const imageQuote = document.getElementById("image-quote");
 
-const div = document.getElementById("unsplash");
+/* the pormises way */
+
+// if (div) {
+//   div.addEventListener("click", () => {
+//     fetch(`https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=${apiKey}`)
+//       .then(response => response.json())
+//       .then(data => {
+//         data = data.results;
+//         let output = "";
+//         data.forEach(image => {
+//           if (image.width > image.height) {
+//             output = `<div class="image"><img src="${image.urls.regular}"></div>`;
+//             document.querySelector(".grid").innerHTML += output;
+//           }
+//         });
+//       })
+//        .catch(err => {
+//      console.log(err);
+//        })
+//   });
+// }
+
+/* the async await way */
+
+const getImages = async () => {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=${apiKey}`
+    );
+    const data = await response.json();
+    const dataResults = data.results;
+    let output = "";
+    dataResults.forEach(image => {
+      if (image.width > image.height) {
+        output = `<div class="image"><img src="${image.urls.regular}"></div>`;
+        grid.innerHTML += output;
+      }
+    });
+  } catch (err) {
+    throw new Error(` your data is corrupted ${err}`);
+  }
+};
+
 if (div) {
-  div.addEventListener("click", () => {
-    fetch(`https://api.unsplash.com/search/photos?query=chicago&per_page=50&client_id=${apiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        data = data.results;
-        let output = "";
-        data.forEach(image => {
-          if (image.width > image.height) {
-            output = `<div class="image"><img src="${image.urls.regular}"></div>`;
-            document.querySelector(".grid").innerHTML += output;
-          }
-        });
-      });
-  });
+  div.addEventListener("click", getImages);
 }
