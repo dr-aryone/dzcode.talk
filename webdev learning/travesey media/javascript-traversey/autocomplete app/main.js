1690a56fdc44c36864516bde2fda5f994a6a8f2e
Zed-M <fsociety.2017@protonmail.com>
2019-04-28 08:24:17 +0100

    adding comments

--- webdev learning/travesey media/javascript-traversey/autocomplete app/main.js
+++ webdev learning/travesey media/javascript-traversey/autocomplete app/main.js
@@ -1,7 +1,10 @@
 const search = document.getElementById('search')
 const searchList = document.getElementById('match-list')
+// output the results to the html using map and join
+// map will take the html body given and return as an array of html elements
+// then join them to a string and ofc an innerhtml at the end
 const outputHtml = matches => {
-  if (matches.length > 0) {
+  if (matches) {
     const html = matches
       .map(
         match => `
@@ -24,9 +27,11 @@ const searchItems = async searchText => {
   const res = await fetch('data.json')
   const data = await res.json()
   let matches = data.filter(state => {
+    // g = global , i = case sensitive
     const regex = new RegExp(`^${searchText}`, 'gi')
     return state.name.match(regex) || state.abbr.match(regex)
   })
+  // reset the input search
   if (searchText.length === 0) {
     matches = []
     searchList.innerHTML = ''
@@ -34,5 +39,5 @@ const searchItems = async searchText => {
 
   outputHtml(matches)
 }
-
+// search.value to get the value typed in the input
 search.addEventListener('input', () => searchItems(search.value))
