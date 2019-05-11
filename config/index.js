43f25d9906af040390f9a1d0e11523d738f78549
Zed-M <fsociety.2017@protonmail.com>
2019-05-11 06:45:29 +0100

    adding snippets

--- config/index.js
+++ config/index.js
@@ -1,3 +1,19 @@
-const eslintrc = require('./.eslintrc');
+const eslintrc = require('./.eslintrc')
+const jshintrc = require('./.jshintrc')
+const prettierrc = require('./.prettierrc')
+const keybindings = require('./keybindings.json')
+const html = require('./html.json')
+const css = require('./css.json')
+const vscJs = require('./vsc_js_snippets.json')
+const tslint = require('./tslint.json')
 
-module.exports = eslintrc;
+module.exports = {
+  eslintrc,
+  jshintrc,
+  prettierrc,
+  keybindings,
+  html,
+  css,
+  vscJs,
+  tslint
+}
