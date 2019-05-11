71ebe952d0f9cf41c9b26a0ec2693e6f43928a04
Zed-M <fsociety.2017@protonmail.com>
2019-05-11 07:04:02 +0100

    adding config files to indexjs

--- config/index.js
+++ config/index.js
@@ -1,11 +1,17 @@
 const eslintrc = require('./.eslintrc')
 const jshintrc = require('./.jshintrc')
 const prettierrc = require('./.prettierrc')
+const babelrc = require('./.babelrc')
 const keybindings = require('./keybindings.json')
 const html = require('./html.json')
 const css = require('./css.json')
 const vscJs = require('./vsc_js_snippets.json')
 const tslint = require('./tslint.json')
+const editorconfig = require('./.editorconfig')
+const env = require('./.env')
+const hyper = require('./.hyper')
+const jest = require('./jest.config')
+const enzyme = require('./enzyme.config')
 
 module.exports = {
   eslintrc,
@@ -15,5 +21,11 @@ module.exports = {
   html,
   css,
   vscJs,
-  tslint
+  tslint,
+  babelrc,
+  editorconfig,
+  env,
+  hyper,
+  jest,
+  enzyme
 }
